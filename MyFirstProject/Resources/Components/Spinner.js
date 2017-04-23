ce55203b22487572e3782cfc430b96707f74f4ef
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2017-04-22 23:34:29 -0500

    tweaked scripts

--- MyFirstProject/Resources/Components/Spinner.js
+++ MyFirstProject/Resources/Components/Spinner.js
@@ -1,7 +1,8 @@
 "atomic component";
 
 var input = Atomic.getInput();
-input.setMouseVisible(false);
+var vis = false
+
 
 var inspectorFields = {
   mouse_damper: 1.0
@@ -10,10 +11,14 @@ var inspectorFields = {
 exports.component = function(self) {
 
   self.update = function(timeStep) {
-
-    self.node.yaw(input.getMouseMoveX()/self.mouse_damper,Atomic.TransformSpace.TS_WORLD);
-    self.node.pitch(input.getMouseMoveY()/self.mouse_damper,Atomic.TransformSpace.TS_LOCAL);
-
+    if(Atomic.input.getKeyDown(Atomic.KEY_SPACE)) {
+        vis? vis = false : vis = true;
+    }
+    input.setMouseVisible(vis);
+    if(!vis) {
+        self.node.yaw(input.getMouseMoveX()/self.mouse_damper,Atomic.TransformSpace.TS_WORLD);
+        self.node.pitch(input.getMouseMoveY()/self.mouse_damper,Atomic.TransformSpace.TS_LOCAL);
+    }
     if(Atomic.input.getKeyDown(Atomic.KEY_ESCAPE)) {
         Atomic.getEngine().exit();
     }
