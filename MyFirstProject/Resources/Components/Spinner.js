4143412835591654c6097a262a2f39ab26217147
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2017-04-22 23:55:05 -0500

    press space to relax...

--- MyFirstProject/Resources/Components/Spinner.js
+++ MyFirstProject/Resources/Components/Spinner.js
@@ -1,8 +1,7 @@
 "atomic component";
 
 var input = Atomic.getInput();
-var vis = false
-
+input.setMouseVisible(false);
 
 var inspectorFields = {
   mouse_damper: 1.0
@@ -11,14 +10,8 @@ var inspectorFields = {
 exports.component = function(self) {
 
   self.update = function(timeStep) {
-    if(Atomic.input.getKeyDown(Atomic.KEY_SPACE)) {
-        vis? vis = false : vis = true;
-    }
-    input.setMouseVisible(vis);
-    if(!vis) {
-        self.node.yaw(input.getMouseMoveX()/self.mouse_damper,Atomic.TransformSpace.TS_WORLD);
-        self.node.pitch(input.getMouseMoveY()/self.mouse_damper,Atomic.TransformSpace.TS_LOCAL);
-    }
+    self.node.yaw(input.getMouseMoveX()/self.mouse_damper,Atomic.TransformSpace.TS_WORLD);
+    self.node.pitch(input.getMouseMoveY()/self.mouse_damper,Atomic.TransformSpace.TS_LOCAL);
     if(Atomic.input.getKeyDown(Atomic.KEY_ESCAPE)) {
         Atomic.getEngine().exit();
     }
